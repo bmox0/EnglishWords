2a6e8d5124f9e1d477856ec0a4183def6b47b7be
bmox0 📱 <rideonroot@gmail.com>
2026-09-19 15:26:46 +1000

    chore: set up Vite, Vue 3, TypeScript and Vitest

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  $schema: "https://json.schemastore.org/prettierrc",
+  arrowParens: "always",
+  bracketSpacing: false,
+  printWidth: 150,
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+}
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import vue from "@vitejs/plugin-vue"
+import {defineConfig} from "vitest/config"
+
+export default defineConfig({
+  base: "./",
+  plugins: [vue()],
+  test: {
+    include: ["src/**/*.test.ts"],
+  },
+})
